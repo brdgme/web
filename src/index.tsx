741db6ca0f8c2b397e9fa94c83f0db1b6a4e3af5
Michael Alexander <beefsack@gmail.com>
2017-03-24 15:31:40 +1100

    Added router

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,27 @@
 import * as React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+} from 'react-router-dom';
 import * as ReactDOM from "react-dom";
 import './style.less';
 
 import { Login } from "./components/Login";
 
+const IndexRoute = () => <Redirect to={{
+  pathname: '/login'
+}} />;
+
+const LoginRoute = () => <Login />;
+
 ReactDOM.render(
-  <Login />,
+  <Router>
+    <div>
+      <Route path="/login" component={LoginRoute} />
+      <Route exact path="/" component={IndexRoute} />
+    </div>
+  </Router>,
   document.getElementById("brdgme")
 );
